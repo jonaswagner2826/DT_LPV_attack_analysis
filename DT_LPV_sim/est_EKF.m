ff36d8a43cec0e0cb18831f1ed130546cb9057e1
jonaswagner2826 <jonas.wagner.2826@gmail.com>
2021-05-12 08:24:11 -0500

    MECH6327Project: Minimal Code Modification... A lot of work done on report/ laying out algorithms.

--- DT_LPV_sim/est_EKF.m
+++ DT_LPV_sim/est_EKF.m
@@ -1,24 +1,56 @@
-function [x_hat, alpha_hat, P] = est_EKF(x_hat, alpha_hat, P, y, u)
+function [x_hat, alpha_hat, P] = est_EKF(x_hat, alpha_hat, P, y, u,...
+                                         A, B, C, D, Q, R)
     %est_EKF Function performs a single iteration of the parameter and
     %state estimation using an EKF
     % The specific Matrices need to be updated based on the system... might
     % update to be parameters later
     
+    arguments
+        x_hat
+        alpha_hat
+        P
+        y
+        u
+        A = string(-1)
+        B = string(-1)
+        C = [1, 0]
+        D = 0
+        Q = string(-1)
+        R = string(-1)
+    end
+    
     % System Matrices
-    n = 2;
-    m = 4;
-    p = 1;
-    q = 1;
-    A(:,:,1) = [-0.80, 0.25; 0.25,-0.30]; B(:,1) = [ 1.90; 0.00];
-    A(:,:,2) = [ 0.30, 0.70; 0.70, 0.00]; B(:,2) = [-1.00; 1.50];
-    A(:,:,3) = [-0.30, 0.65; 0.55, 0.10]; B(:,3) = [ 0.30;-2.00];
-    A(:,:,4) = [ 0.55,-0.20;-0.40,-0.30]; B(:,4) = [-0.60; 0.00];
-    C = [1, 0]; D = 0;
+    if string(A) == string(-1)
+        clear A
+        A(:,:,1) = [-0.80, 0.25; 0.25,-0.30];
+        A(:,:,2) = [ 0.30, 0.70; 0.70, 0.00];
+        A(:,:,3) = [-0.30, 0.65; 0.55, 0.10];
+        A(:,:,4) = [ 0.55,-0.20;-0.40,-0.30];
+    end
+    if string(B) == string(-1)
+        clear B
+        B(:,1) = [ 1.90; 0.00];
+        B(:,2) = [-1.00; 1.50];
+        B(:,3) = [ 0.30;-2.00];
+        B(:,4) = [-0.60; 0.00];
+    end
+    
+    % System Dimenstion Matrices
+    n = size(A,1);
+    m = size(A,3);
+    p = size(B,1);
+    q = size(C,1);
     
     
     % EKF Covariance Matrices
-    Q = diag([zeros(1,n), 100*ones(1,m)]);
-    R = 0.01;
+    if Q == string(-1)
+        clear Q
+        Q = diag([zeros(1,n), 100*ones(1,m)]);
+    end
+    if R == string(-1)
+        clear R
+        R = 0.01;
+    end
     
     % A_hat (k-1) calc
     A_hat = diag([zeros(1,n), ones(1,m)]);
